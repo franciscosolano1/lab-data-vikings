834437aa170017d5ec4dd8e09418330f5b9a70ad
franciscosolano1 <88449161+franciscosolano1@users.noreply.github.com>
2021-11-01 10:37:30 -0600

    viking

--- lab-data-vikings/vikingsClases.py
+++ lab-data-vikings/vikingsClases.py
@@ -1,8 +1,13 @@
-
+import testsSoldier
 # Soldier
 
 
 class Soldier:
+    def __init__(health,strength):
+        health = health
+        strength=strength
+        return health,strength
+
     pass
 
 # Viking
